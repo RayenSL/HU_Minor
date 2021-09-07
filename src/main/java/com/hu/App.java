66b4762308d565b726b9336857879b7f5720c239
rayen <rayensyal@outlook.com>
2021-09-07 17:11:56 +0200

    [TASK] Initialized Maven Project

---
+++ src/main/java/com/hu/App.java
@@ -0,0 +1,4 @@
+package com.hu;
+
+public class App {
+}
---
+++ src/main/java/com/hu/ApplicationContext.java
@@ -0,0 +1,9 @@
+package com.hu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configuration
+@ComponentScan(basePackages = "com.hu")
+public class ApplicationContext {
+}
