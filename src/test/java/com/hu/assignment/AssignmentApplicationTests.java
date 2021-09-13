6903800a809f000fba1b07fd1483979d5341e992
rayen <rayensyal@outlook.com>
2021-09-13 21:21:20 +0200

    [TASK] Updated dependency's/plugins

--- src/test/java/com/hu/assignment/AssignmentApplicationTests.java
+++ src/test/java/com/hu/assignment/AssignmentApplicationTests.java
@@ -1,10 +1,16 @@
 package com.hu.assignment;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AssignmentApplicationTests {
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("@BeforeAll");
+    }
 
     @Test
     void contextLoads() {
